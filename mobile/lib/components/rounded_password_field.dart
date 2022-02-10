fc7785a256820f41ef32333d9590d18d3663a003
haikallf <haikallzrd@gmail.com>
2022-02-10 18:07:12 +0700

    hide and show password, pubspec asset fix

--- mobile/lib/components/rounded_password_field.dart
+++ mobile/lib/components/rounded_password_field.dart
@@ -2,7 +2,7 @@ import 'package:cakrawala_mobile/components/text_field_container.dart';
 import 'package:cakrawala_mobile/constants.dart';
 import "package:flutter/material.dart";
 
-class RoundedPasswordField extends StatelessWidget {
+class RoundedPasswordField extends StatefulWidget {
   final ValueChanged<String> onChanged;
   const RoundedPasswordField({
     Key? key,
@@ -10,10 +10,25 @@ class RoundedPasswordField extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  RoundedPasswordFieldState createState() => RoundedPasswordFieldState();
+}
+
+class RoundedPasswordFieldState extends State<RoundedPasswordField> {
+  bool _isVisible = false;
+
+  void handleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return TextFieldContainer(
         child: TextField(
-      obscureText: true,
+      obscureText: _isVisible ? false : true,
+      onChanged: widget.onChanged,
+      style: TextStyle(color: Colors.white),
       decoration: InputDecoration(
           hintText: "Password",
           hintStyle: TextStyle(color: Colors.white),
@@ -21,9 +36,14 @@ class RoundedPasswordField extends StatelessWidget {
             Icons.lock,
             color: primaryColor,
           ),
-          suffixIcon: Icon(
-            Icons.visibility,
-            color: primaryColor,
+          suffixIcon: IconButton(
+            onPressed: () => handleVisibility(),
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            icon: Icon(
+              _isVisible ? Icons.visibility : Icons.visibility_off,
+              color: primaryColor,
+            ),
           ),
           border: InputBorder.none),
     ));
