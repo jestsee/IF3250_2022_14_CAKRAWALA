f34c86870aa544348da1bdd067b2a346c4ddef7c
hafid abi d <abi.daniswara@gmail.com>
2022-02-13 12:01:08 +0700

    fix middleware
    Former-commit-id: e3fa43e2fdec1052054e295b86b80def81a86c69

--- backend/middleware/server/go.mod
+++
@@ -1,5 +0,0 @@
-module jwt_server
-
-go 1.17
-
-require github.com/dgrijalva/jwt-go v3.2.0+incompatible
--- backend/routes/userroutes.go
+++ backend/routes/userroutes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"cakrawala.id/m/controllers"
+	"cakrawala.id/m/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,5 +11,10 @@ func userRoutes(e *gin.Engine) {
 	{
 		gr.POST("/register", controllers.Register)
 		gr.POST("/login", controllers.Login)
+		gr.GET("/test", middleware.IsAuthrorized(), func(context *gin.Context) {
+			context.JSON(200, gin.H{
+				"message": "tampan dan pemberani",
+			})
+		})
 	}
 }
