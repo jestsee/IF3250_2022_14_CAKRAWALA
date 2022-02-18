1037ba693f08e68bd5ecfa434f3489880e0beeae
haikallf <haikallzrd@gmail.com>
2022-02-18 19:11:11 +0700

    dynamic size of profile picture, button color with constant
    Former-commit-id: 9beb46e6a241ce6e27c89ce6b36aa7d9704c33c8

--- mobile/lib/components/bottom_confirm_button.dart
+++ mobile/lib/components/bottom_confirm_button.dart
@@ -10,31 +10,28 @@ class ButtonConfirmButton extends StatelessWidget {
   final Color color, textColor;
   const ButtonConfirmButton(
       {Key? key,
-        required this.text,
-        required this.press,
-        this.color = primaryColor,
-        this.textColor = Colors.white})
+      required this.text,
+      required this.press,
+      this.color = primaryColor,
+      this.textColor = white})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery
-        .of(context)
-        .size; // Screen height and width
+    Size size = MediaQuery.of(context).size; // Screen height and width
 
-      return Expanded(
+    return Expanded(
         child: Align(
-          alignment: FractionalOffset.bottomCenter,
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 0.02 * size.height),
-            child:
-            RoundedButton(
-              text: text,
-              press: press,
-              color: Colors.black,
-            ),
-          ),
-        )
-      );
-    }
+      alignment: FractionalOffset.bottomCenter,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 0.02 * size.height),
+        child: RoundedButton(
+          text: text,
+          press: press,
+          color: black,
+          textColor: white,
+        ),
+      ),
+    ));
   }
+}
