6122f687ce29e053652d4fbbbc4bfe7675c3c760
haikallf <haikallzrd@gmail.com>
2022-02-18 18:17:42 +0700

    match login screen color with figma
    Former-commit-id: 42a387b242189142ade3b5b8fce12709a7ea2751

--- mobile/lib/components/text_field_container copy.dart
+++ mobile/lib/components/text_field_container copy.dart
@@ -16,8 +16,8 @@ class TextFieldContainer extends StatelessWidget {
       margin: EdgeInsets.symmetric(vertical: 10),
       padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
       width: size.width * 0.8,
-      decoration: BoxDecoration(
-          color: primaryLightColor, borderRadius: BorderRadius.circular(29)),
+      decoration:
+          BoxDecoration(color: black, borderRadius: BorderRadius.circular(29)),
       child: child,
     );
   }
