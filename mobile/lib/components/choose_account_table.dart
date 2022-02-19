0ce68b6f70fd839c72b9499af32d324531099aaf
Jesica <13519011@std.stei.itb.ac.id>
2022-02-19 20:29:39 +0700

    add search function
    Former-commit-id: 6ad3fbe89b65ed59dbbc60db4dfa5cac31366093

--- mobile/lib/components/choose_account_table.dart
+++ mobile/lib/components/choose_account_table.dart
@@ -1,15 +1,33 @@
 import 'dart:developer';
-
+import 'package:cakrawala_mobile/components/search_box.dart';
 import 'package:cakrawala_mobile/components/text_account_attribute.dart';
 import 'package:cakrawala_mobile/constants.dart';
 import 'package:flutter/material.dart';
 
-class ChooseAccountTable extends StatelessWidget {
+class User {
+  String name;
+  String phone;
+  int exp;
+
+  User(this.name, this.phone, this.exp);
+  factory User.fromJson(dynamic json) {
+    return User(
+        json['name'] as String,
+        json['phone'] as String,
+        json['exp'] as int);
+  }
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return '{${this.name}, ${this.phone}, ${this.exp}}';
+  }
+}
+
+class ChooseAccountTable extends StatefulWidget {
   ChooseAccountTable({Key? key}) : super(key: key);
 
-  final rows = <TableRow>[];
   static const dummyData = [
-    {"name": "Sasasdasdhasfadtu", "phone": "0812345670", "exp": 15000},
+    {"name": "Zerso", "phone": "0812345670", "exp": 15000},
     {"name": "Satu", "phone": "0812345671", "exp": 18000},
     {"name": "Dua", "phone": "0812345672", "exp": 25000},
     {"name": "Satu", "phone": "0812345670", "exp": 15000},
@@ -17,100 +35,87 @@ class ChooseAccountTable extends StatelessWidget {
     {"name": "Dua", "phone": "0812345672", "exp": 25000},
   ];
 
-  void dataToTable(size) {
-    for (var data in dummyData) {
-      log('data: $data');
-      rows.add(TableRow(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: 0.1 * size.height
-            ),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10.0),
-                child:
-                Image.network(
-                  'https://picsum.photos/250?image=9',
-                  height: 0.13 * size.width,
-                  width: 0.13 * size.width,
-                ),
-              ),
-            ),
-          ),
-          Text(
-            data['name'].toString(),
-            textAlign: TextAlign.left,
-            style: TextStyle (
-                fontWeight: FontWeight.w800,
-                fontSize: 16,
-                color: Colors.white
-            ),
-          ),
-          Text(
-            data['phone'].toString(),
-            textAlign: TextAlign.left,
-            style: TextStyle (
-                fontWeight: FontWeight.w700,
-                fontSize: 13,
-                color: Colors.white
-            ),
-          ),
-          Text(
-            data['exp'].toString(),
-            textAlign: TextAlign.right,
-            style: TextStyle (
-                fontWeight: FontWeight.w700,
-                fontSize: 14,
-                color: Colors.white
-            ),
-          ),
-        ],
-      ));
-    }
+  @override
+  State<ChooseAccountTable> createState() => _ChooseAccountTableState();
+}
+
+class _ChooseAccountTableState extends State<ChooseAccountTable> {
+  List<User> users = ChooseAccountTable.dummyData.map((e) =>
+      User.fromJson(e)).toList();
+  List<User> usersFiltered = [];
+  TextEditingController controller = TextEditingController();
+  String _searchResult = '';
+  
+  @override
+  void initState() {
+    super.initState();
+    usersFiltered = users;
   }
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size; // Screen height and width
     double rowHeight = 0.05 * size.height;
+    log('data: $_searchResult');
     // dataToTable(size);
 
-    return DataTable(
-      border: TableBorder.all(color: primaryBlue),
-      dividerThickness: 0,
-      dataRowHeight: 0.12 * size.height,
-      headingRowHeight: 0,
-      horizontalMargin: 0,
-      columnSpacing: 0,
-      columns: const <DataColumn> [
-        DataColumn(label: Text('a')),
-        DataColumn(label: Text('asd')),
-        DataColumn(label: Text('asd')),
-        DataColumn(label: Text('asd')),
-      ], rows: dummyData.map((e) {
-        return DataRow(cells: <DataCell>[
-          DataCell(
-            Align (
-              alignment: Alignment.centerLeft,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10.0),
-                child:
-                Image.network(
-                  'https://picsum.photos/250?image=9',
-                  height: 0.13 * size.width,
-                  width: 0.13 * size.width,
-                ),
-              ),
+    return Column(
+      children: [
+        SearchBox(
+            hintText: 'Search account', 
+            onChanged: (value) {
+              setState(() {
+                _searchResult = value;
+                usersFiltered = users.where((user) =>
+                    (user.name.toLowerCase().contains(_searchResult))||
+                        user.phone.contains(_searchResult)).toList();
+              });
+            }
+        ),
+        Container(
+          width: .8 * size.width,
+          height: .6598 * size.height,
+          child: SingleChildScrollView (
+            child: DataTable(
+                border: TableBorder.all(color: primaryBlue),
+                dividerThickness: 0,
+                dataRowHeight: 0.12 * size.height,
+                headingRowHeight: 0,
+                horizontalMargin: 0,
+                columnSpacing: 0,
+                columns: const <DataColumn> [
+                  DataColumn(label: Text('a')),
+                  DataColumn(label: Text('asd')),
+                  DataColumn(label: Text('asd')),
+                  DataColumn(label: Text('asd')),
+                ],
+                rows: List.generate(usersFiltered.length, (index) =>
+                  DataRow(
+                    cells: <DataCell>[
+                      DataCell(
+                            Align (
+                              alignment: Alignment.centerLeft,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10.0),
+                                child:
+                                Image.network(
+                                  'https://picsum.photos/250?image=9',
+                                  height: 0.13 * size.width,
+                                  width: 0.13 * size.width,
+                                ),
+                              ),
+                            ),
+                          ),
+                          DataCell(TextName(text: usersFiltered[index].name)),
+                          DataCell(TextPhone(text:  usersFiltered[index].phone)),
+                          DataCell(TextExp(text:  usersFiltered[index].exp.toString())),
+                    ]
+                  )
+                )
             ),
           ),
-          DataCell(TextName(text:  e['name'].toString())),
-          DataCell(TextPhone(text:  e['phone'].toString())),
-          DataCell(TextExp(text:  e['exp'].toString())),
-        ]
-        );
-    }).toList()
+        ),
+      ],
     );
   }
 }
