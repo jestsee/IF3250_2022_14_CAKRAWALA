0dbb1eb1dc0200018f5720b31f4199c882cae031
hafid abi d <abi.daniswara@gmail.com>
2022-02-20 22:19:26 +0700

    asdf
    Former-commit-id: 52e23dcc2797247bce72fac96bd3802e986c55fa

--- backend/routes/userroutes.go
+++ backend/routes/userroutes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"cakrawala.id/m/controllers"
+	"cakrawala.id/m/controllers/transactions"
 	"cakrawala.id/m/middleware"
 	"github.com/gin-gonic/gin"
 )
@@ -9,12 +10,14 @@ import (
 func userRoutes(e *gin.Engine) {
 	gr := e.Group("/v1")
 	{
-		gr.POST("/register", controllers.Register)
+		gr.POST("/register", middleware.CheckRegister(), controllers.Register)
 		gr.POST("/login", controllers.Login)
 		gr.GET("/test", middleware.IsAuthrorized(), func(context *gin.Context) {
 			context.JSON(200, gin.H{
 				"message": "tampan dan pemberani",
 			})
 		})
+
+		gr.POST("/top-up", middleware.IsAuthrorized(), transactions.TopUpRequest)
 	}
 }
