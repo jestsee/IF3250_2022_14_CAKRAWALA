f04119bd97a04cd456288b519167004ee39e8e8a
hafid abi d <abi.daniswara@gmail.com>
2022-03-27 23:23:51 +0700

    add unittest for login register
    Former-commit-id: a31eab153eac0fde5377a241165b7d35d767353d

---
+++ backend/controllers/test/setup_test.go
@@ -0,0 +1,79 @@
+package test
+
+import (
+	"bytes"
+	"cakrawala.id/m/controllers"
+	"cakrawala.id/m/models"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/suite"
+	"gorm.io/gorm"
+	"net/http"
+	"testing"
+)
+
+type TestSuiteEnv struct {
+	suite.Suite
+	db *gorm.DB
+}
+
+// Tests are run before they start
+func (suite *TestSuiteEnv) SetupSuite() {
+	models.ConnectDatabase()
+	suite.db = models.DB
+}
+
+// Running after each test
+func (suite *TestSuiteEnv) TearDownTest() {
+	models.ClearTable()
+}
+
+// Running after all tests are completed
+func (suite *TestSuiteEnv) TearDownSuite() {
+	//suite.db.Close()
+}
+
+// This gets run automatically by `go test` so we call `suite.Run` inside it
+func TestSuite(t *testing.T) {
+	// This is what actually runs our suite
+	suite.Run(t, new(TestSuiteEnv))
+}
+
+
+func (suite *TestSuiteEnv) TestLoginRegister{
+	r:= gin.New()
+	r.POST("/register", controllers.Register)
+	var jstr = []byte(`{
+    "email" : "13519002@std.stei.itb.ac.id",
+    "name" : "robert alfonsius",
+    "phone" : "085257560828",
+    "password" : "asdf12345"
+	}`)
+	req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(jstr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	a:=suite.Assert()
+	a.Equal(resp.StatusCode, 200)
+
+	req, err = http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(jstr))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err = client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	a.Equal(resp.StatusCode, 400)
+
+}
+
+func (suite *TestSuiteEnv) TestMiddleware{
+
+}
--- backend/go.mod
+++ backend/go.mod
@@ -4,8 +4,10 @@ go 1.13
 
 require (
 	github.com/gin-gonic/gin v1.7.7
+	github.com/go-pg/pg/v10 v10.10.6 // indirect
 	github.com/golang-jwt/jwt/v4 v4.0.0
 	github.com/joho/godotenv v1.4.0
+	github.com/steinfletcher/apitest v1.5.11 // indirect
 	github.com/swaggo/gin-swagger v1.4.1
 	github.com/swaggo/swag v1.7.9
 	github.com/urfave/cli v1.20.0 // indirect
