5aaf0a6790b7c492b12c811611c74a0f61ad733b
haikallf <haikallzrd@gmail.com>
2022-03-27 13:42:29 +0700

    add transfer function
    Former-commit-id: 93e0245fce4370effaa03185c020fc2c872eda89

--- backend/controllers/transactions/transfer.go
+++ backend/controllers/transactions/transfer.go
@@ -1,51 +1,83 @@
 package transactions
 
-// import (
-// 	"net/http"
+import (
+	"log"
+	"net/http"
 
-// 	"cakrawala.id/m/models"
-// 	"cakrawala.id/m/service"
-// 	"cakrawala.id/m/utils"
-// 	"github.com/gin-gonic/gin"
-// )
+	"cakrawala.id/m/models"
+	"cakrawala.id/m/utils"
+	"github.com/gin-gonic/gin"
+)
 
 
-// type TransferBody struct {
-// 	Amount 		uint64 	`json:"amount`
-// 	Exp 		uint32 	`json:"exp"`
-// 	Status 		string 	`json:"status"`
-// 	Cashback 	uint32 	`json:"cashback"`
-// 	UserID 		uint 	`json:"user_id"`
-// 	FriendID 	uint 	`json:"friend_id"`
-// }
+type TransferBody struct {
+	Amount 		uint64 	`json:"amount"`
+	Exp 		uint32 	`json:"exp"`
+	Status 		string 	`json:"status"`
+	Cashback 	uint32 	`json:"cashback"`
+	UserID 		uint 	`json:"user_id"`
+	FriendID 	uint 	`json:"friend_id"`
+}
 
-// func Transfer(c *gin.Context)  {
-// 	user := c.MustGet("user").(models.User)
-// 	data := new(PayMerchantBody)
+func Transfer(c *gin.Context)  {
+	user := c.MustGet("user").(models.User)
+	var data = new(TransferBody)
+	// var sender_phone = "081219083250"
+	var receiver_phone = "081219083251"
+	var receiver models.User
 
-// 	if err := c.ShouldBind(data); err != nil {
-// 		c.JSON(http.StatusBadRequest, utils.ErrorResponse(err))
-// 		return
-// 	}
+	if err := c.ShouldBind(data); err != nil {
+		c.JSON(http.StatusBadRequest, utils.ErrorResponse(err))
+		return
+	}
 
-// 	bonus := service.TransferBonus(user, int64(data.Amount))
-// 	transaction := models.Transaksi{
-// 		Amount: data.Amount,
-// 		Exp: uint32(bonus),
-// 		Cashback: uint32(5),
-// 		UserID: data.UserID,
-// 		MerchantID: nil,
-// 		FriendID: &data.UserID,
-// 	}
+	// check receiver's phone
+	err := models.DB.Where("phone = ?", receiver_phone).First(&receiver).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"message": "no ponsel tidak ada",
+		})
+		c.Abort()
+		return
+	}
 
-// 	err := models.DB.Create(&transaction).Error
-// 	if err != nil {
-// 		_ = c.AbortWithError(500, err)
-// 		return
-// 	}
+	// check sender's balance
+	if user.Balance < data.Amount {
+		c.JSON(400, gin.H{
+			"message": "saldo kurang",
+		})
+		c.Abort()
+		return
+	}
 
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"message": "berhasil transfer",
-// 		"data":    transaction,
-// 	})
-// }
+	user.Balance -= uint64(data.Amount)
+	receiver.Balance += uint64(data.Amount)
+
+	models.DB.Updates(&user)
+	models.DB.Updates(&receiver)
+	
+
+
+
+
+	// bonus := service.TransferBonus(user, int64(data.Amount))
+	transaction := models.Transaksi{
+		Amount: data.Amount,
+		Exp: uint32(10),
+		Cashback: uint32(5),
+		UserID: data.UserID,
+		MerchantID: nil,
+		FriendID: &data.UserID,
+	}
+	log.Println(transaction)
+
+	// err := models.DB.Create(&transaction).Error
+	// if err != nil {
+	// 	_ = c.AbortWithError(500, err)
+	// 	return
+	// }
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "berhasil transfer",
+	})
+}
