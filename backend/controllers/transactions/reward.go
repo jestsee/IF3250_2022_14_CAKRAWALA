fabd7ead5e890ffc85c75dadac6fa634a2921e72
Jesica <13519011@std.stei.itb.ac.id>
2022-03-27 19:07:37 +0700

    add exchange reward
    Former-commit-id: 02b3fce4867f998eedbfb4f3ad341b1f27cff1ef

--- backend/controllers/transactions/reward.go
+++ backend/controllers/transactions/reward.go
@@ -14,6 +14,12 @@ type AddRewardBody struct {
 	Stock	uint16	`json:"stock"`
 }
 
+type ExchangeRewardBody struct {
+	RewardID	uint	`json:"reward_id"`
+	Quantity	uint16 	`json:"qty"`
+	PointsPaid	uint32	`json:"paid"`
+}
+
 func AddReward(c *gin.Context) {
 	var body AddRewardBody
 	if err := c.BindJSON(&body); err != nil {
@@ -44,48 +50,57 @@ func AddReward(c *gin.Context) {
 	}
 }
 
-// func RewardController(c *gin.Context) {
-// 	// get user
-// 	user := c.MustGet("user").(models.User)
-// 	var body AddRewardBody
-// 	if err := c.BindJSON(&body); err != nil {
-// 		c.AbortWithError(400, err)
-// 		return
-// 	}
+func ExchangeReward(c *gin.Context) {
+	// get user
+	user := c.MustGet("user").(models.User)
+	var body ExchangeRewardBody
+	if err := c.BindJSON(&body); err != nil {
+		c.AbortWithError(400, err)
+		return
+	}
 
-// 	// cek point cukup ga
-// 	if user.Point < body.Point {
-// 		c.AbortWithStatusJSON(400, gin.H{
-// 			"message": "insufficient points",
-// 		})
-// 		return
-// 	}
+	// cek point cukup ga
+	if user.Point < body.PointsPaid {
+		c.AbortWithStatusJSON(400, gin.H{
+			"message": "insufficient points",
+		})
+		return
+	}
 
-// 	reward := models.Reward {
-// 		Name: 	body.Name,
-// 		Price:	body.Point,
-// 		Stock:	body.Stock,
-// 	}
+	reward_history := models.HistoryReward {
+		Quantity: 	body.Quantity,
+		PointsPaid: body.PointsPaid,
+		UserID: 	user.ID,
+		RewardID:	body.RewardID,
+	}
 
-// 	user.Point -= body.Point
-// 	println(user.Point)
-// 	err := models.DB.Transaction(func(tx *gorm.DB) error {
-// 		if e:= tx.Save(&reward).Error; e != nil {
-// 			return e
-// 		}
-// 		if e:= tx.Updates(&user).Error; e != nil {
-// 			return e
-// 		}
-// 		return nil
-// 	})
+	// kurangin point user
+	user.Point -= body.PointsPaid
 
-// 	if err == nil {
-// 		c.JSON(http.StatusOK, gin.H{
-// 			"message": "berhasil  menukarkan point",
-// 			"data":    reward,
-// 		})
-// 	} else {
-// 		_ = c.AbortWithError(500, err)
-// 	}
+	// kurangin stock reward
 
-// }
+
+	err := models.DB.Transaction(func(tx *gorm.DB) error {
+		// add row baru di history
+		if e:= tx.Save(&reward_history).Error; e != nil {
+			return e
+		}
+		// update point user
+		if e:= tx.Updates(&user).Error; e != nil {
+			return e
+		}
+		// update stock reward
+		// if e:= tx.Update()
+		return nil
+	})
+
+	if err == nil {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "berhasil  menukarkan point",
+			"data":    reward_history,
+		})
+	} else {
+		_ = c.AbortWithError(500, err)
+	}
+
+}
