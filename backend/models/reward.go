2ac16e0aa88cd2b835fc9c9eef7dfb7c7e8a082e
hafid abi d <abi.daniswara@gmail.com>
2022-03-30 21:20:22 +0700

    solve decreasing error problem
    Former-commit-id: 41c637e22325b340318d927076796e0ad3433869

--- backend/models/reward.go
+++ backend/models/reward.go
@@ -8,7 +8,7 @@ type Reward struct {
 	ID        uint      `gorm:"primary_key;auto_increment" json:"id"`
 	Name      string    `gorm: "not_null"`
 	Price     uint32    `gorm: "not_null"`
-	Stock     uint16    `gorm: "not_null"`
+	Stock     int16     `gorm: "not_null"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 
