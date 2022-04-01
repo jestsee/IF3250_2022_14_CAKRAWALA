98499f8e3d2e4837c5b1b876e935601a7b1f1f03
Jesica <13519011@std.stei.itb.ac.id>
2022-04-01 08:33:15 +0700

    styling
    Former-commit-id: 36f7a2e215ca1bd45c24711b3f381c5f134b16f0

--- mobile/lib/Screens/Homepage/components/history_container.dart
+++ mobile/lib/Screens/Homepage/components/history_container.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../constants.dart';
+
 class HistoryContainer extends StatelessWidget {
   final Widget child;
   const HistoryContainer({
@@ -13,13 +15,14 @@ class HistoryContainer extends StatelessWidget {
 
     return Center(
       child: Container(
-        height: 220,
+        height: 250,
         margin: const EdgeInsets.symmetric(vertical: 10),
         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-        width: size.width * 0.9,
+        width: size.width * 0.89,
         decoration: BoxDecoration(
-            color: Color.fromARGB(255, 52, 146, 223),
-            borderRadius: BorderRadius.circular(10)),
+          // color: Colors.black,
+          border: Border.all(width: 0.6, color: white),
+          borderRadius: BorderRadius.circular(10)),
         child: child,
       ),
     );
