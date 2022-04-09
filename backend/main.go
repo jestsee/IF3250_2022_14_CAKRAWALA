288a227f903002dc0c7f831cf40adfc72f8c5d31
haikallf <haikallzrd@gmail.com>
2022-04-09 21:06:20 +0700

    add cors
    Former-commit-id: fa3753a1d91ed652ff2611f8fb26205fc770e162

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"net/http"
+
 	_ "cakrawala.id/m/docs"
 	"cakrawala.id/m/models"
 	"cakrawala.id/m/routes"
 	"github.com/gin-gonic/gin"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
-	"net/http"
 )
 
 // @title Cakrawala API
@@ -26,6 +27,8 @@ import (
 func main() {
 	r := gin.Default()
 
+	r.Use(CORSMiddleware())
+
 	routes.GetRoutes(r)
 	r.GET("/", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
@@ -39,5 +42,22 @@ func main() {
 	err := r.Run()
 	if err != nil {
 		panic("oiii gabisa jalan aplikasinyaa")
+	}
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
 	}
 }
