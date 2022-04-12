552857aa8b686d4818aa308f4e682d36ee51a0ca
Hafid Abi Daniswara <13519028@std.stei.itb.ac.id>
2022-04-12 16:33:54 +0000

    Issue/30/admin deluser addprize
    Former-commit-id: 800caa69b6d5aae51bcb7893345e676a30eab459

--- backend/routes/adminroutes.go
+++ backend/routes/adminroutes.go
@@ -12,6 +12,8 @@ func adminRoutes(e *gin.Engine) {
 		gr.POST("/login", controllers.Login)
 
 		gr.GET("/user/:id", controllers.GetUserNameById)
+		gr.GET("/user", controllers.GetAllUser)
+		gr.DELETE("/user/:id", controllers.DeleteUser)
 
 		gr.PATCH("/top-up/:id", transactions.ApproveTopUp)
 		gr.GET("/top-up/request", transactions.GetTopUpRequest)
@@ -23,5 +25,7 @@ func adminRoutes(e *gin.Engine) {
 		gr.POST("/merchant/add", transactions.AddMerchant)
 		gr.GET("/merchant", transactions.GetAllMerchantsAdmin)
 		gr.POST("/add-reward", transactions.AddReward)
+		gr.GET("/reward", transactions.GetAllRewards)
+		gr.PATCH("/reward/:id", transactions.UpdateStock)
 	}
 }
