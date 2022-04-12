78bbff4cb4f9a957eca99370e9206763daed6a2e
Jesica <13519011@std.stei.itb.ac.id>
2022-04-12 14:52:13 +0700

    integrate fe be
    Former-commit-id: 986327e521007c63f772400ccad9563448556db1

--- mobile/lib/Screens/Transfer/components/body.dart
+++ mobile/lib/Screens/Transfer/components/body.dart
@@ -1,18 +1,32 @@
 // ignore_for_file: prefer_const_constructors
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:cakrawala_mobile/Screens/Homepage/homepage_screen.dart';
 import 'package:cakrawala_mobile/components/bottom_confirm_button.dart';
 // import 'package:cakrawala_mobile/components/choose_account_table2.dart';
 import 'package:cakrawala_mobile/components/circle_profile_icon.dart';
 import 'package:cakrawala_mobile/components/enter_amount_input.dart';
 import 'package:cakrawala_mobile/components/user_profile_container.dart';
 import 'package:cakrawala_mobile/components/white_text_field_container.dart';
+import 'package:cakrawala_mobile/utils/transfer-api.dart';
 import "package:flutter/material.dart";
+import 'package:fluttertoast/fluttertoast.dart';
 
+import '../../../components/blurry-dialog.dart';
 import '../../../components/choose_account_table1.dart';
+import '../../../constants.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   final User choosenUser;
   const Body({Key? key, required this.choosenUser}) : super(key: key);
 
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  int amount = 0;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size; // Screen height and width
@@ -23,7 +37,7 @@ class Body extends StatelessWidget {
             height: size.height * 0.05,
           ),
           CircleIcon(
-            textName: choosenUser.name,
+            textName: widget.choosenUser.name,
             press: () {},
             textColor: Colors.white,
           ),
@@ -32,9 +46,9 @@ class Body extends StatelessWidget {
           ),
           WhiteFieldContainer(
             child: UserProfileContainer(
-              name: choosenUser.name,
+              name: widget.choosenUser.name,
               address: "",
-              email: choosenUser.email
+              email: widget.choosenUser.email
             )
           ),
           SizedBox(
@@ -42,14 +56,111 @@ class Body extends StatelessWidget {
           ),
           EnterAmountInput(
             hintText: "Enter the amount to transfers",
-            onChanged: (value) {},
+            onChanged: (value) {amount = value == "" ? 0 : int.parse(value);},
           ),
           ButtonConfirmButton(
             text: "Confirm Transfer",
             press: (){
-              currentUser = User(-1, "Unknown", "-1", -1, "Unknown");
+              if (amount == 0) {
+                Fluttertoast.showToast(
+                    msg: "Field amount cannot be empty",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.CENTER,
+                    timeInSecForIosWeb: 1,
+                    backgroundColor: Colors.black54,
+                    textColor: Colors.white,
+                    fontSize: 14.0
+                );
+                return;
+              }
+              // pop up dialog
+              showConfirmDialog(context, () async {
+                var resp = await TransferAPI.transfer(widget.choosenUser.id, amount);
+                if(resp.data) {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Homepage()
+                    )
+                  );
+                  // reset current user
+                  currentUser = User(-1, "Unknown", "-1", -1, "Unknown");
+                } else {
+                  log('resp message: ${resp.message}');
+                  var msg = json.decode(resp.message) as Map<String, dynamic>;
+                  var temp = msg['message'];
+                  _showDialog(context, "Gagal melakukan transfer", temp, null);
+                }
+              });
             }),
         ],
+    );
+  }
+
+  _showDialog(BuildContext context, title, content, callback) {
+    BlurryDialog bd = BlurryDialog(title, content, callback);
+
+    showDialog(context: context, builder: (BuildContext context) {
+      return bd;
+    });
+  }
+
+  showConfirmDialog(BuildContext context, VoidCallback pressContinue) {
+    // set up the buttons
+    Widget cancelButton = ElevatedButton(
+      child: const Text("Cancel",
+        style: TextStyle(
+            color: black,
+            fontWeight: FontWeight.w600
+        ),
+      ),
+      onPressed:  () => Navigator.pop(context),
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(white),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(14),
+                  side: const BorderSide(color: black, width: 0.8)
+              )
+          )
+      ),
+    );
+    Widget continueButton = ElevatedButton(
+        child: const Text(
+            "Continue",
+            style: TextStyle(
+                color: white,
+                fontWeight: FontWeight.w600
+            )
+        ),
+        onPressed: pressContinue,
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(black),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(14),
+                  side: const BorderSide(color: black),
+                )
+            )
+        )
+    );
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15)
+      ),
+      title: const Text("Transfer Confirmation"),
+      content: const Text("Are you sure you want to continue?"),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
     );
   }
 }
