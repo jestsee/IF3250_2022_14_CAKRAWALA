20d68db6eab2c103ee3f132e5d2a9b554b693e61
13519001 Karlsen Adiyasa Bachtiar <13519001@std.stei.itb.ac.id>
2022-04-13 01:25:30 +0700

    load data from API to frontend
    Former-commit-id: e1b63e448015d3d166eb80fb312971d712c51220

--- mobile/lib/Screens/AccountInfo/components/body.dart
+++ mobile/lib/Screens/AccountInfo/components/body.dart
@@ -1,16 +1,52 @@
 import 'package:cakrawala_mobile/components/custom_app_bar.dart';
 import 'package:cakrawala_mobile/constants.dart';
 import 'package:cakrawala_mobile/Screens/AccountInfo/components/custom_text.dart';
+import 'package:cakrawala_mobile/utils/userinfo-api.dart';
 import "package:flutter/material.dart";
 
 class Body extends StatefulWidget {
-  const Body({ Key? key }) : super(key: key);
+  const Body({Key? key}) : super(key: key);
 
   @override
   State<Body> createState() => _BodyState();
 }
 
 class _BodyState extends State<Body> {
+  String email = "-";
+  String name = "-";
+  String phone = "-";
+  String balance = "-";
+  String points = "-";
+  String exp = "-";
+  // String rewards = "-";
+  // TODO reward pake button
+  Map<String, dynamic> userData = {};
+
+  @override
+  void initState() {
+    loadState();
+    super.initState();
+  }
+
+  void loadState() async {
+    await UserInfoAPI.getUserInformation().then((data) {
+      setState(() {
+        if (data.status == 200) {
+          setState(() {
+            email = data.data['email'];
+            name = data.data['Name'];
+            phone = data.data['Phone'];
+            balance = data.data['balance'].toString();
+            points = data.data['point'].toString();
+            exp = data.data['exp'].toString();
+            userData = data.data;
+            print(data.data);
+          });
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -23,27 +59,13 @@ class _BodyState extends State<Body> {
       ),
       body: Column(
         children: const <Widget>[
-          CustomText(
-            textString: "Name", 
-            fontSize: 18),
-          CustomText(
-            textString: "Email", 
-            fontSize: 18),
-          CustomText(
-            textString: "Phone", 
-            fontSize: 18),
-          CustomText(
-            textString: "Balance", 
-            fontSize: 18),
-          CustomText(
-            textString: "Point", 
-            fontSize: 18),
-          CustomText(
-            textString: "EXP", 
-            fontSize: 18),
-          CustomText(
-            textString: "Reward", 
-            fontSize: 18),
+          CustomText(textString: "Name", fontSize: 18),
+          CustomText(textString: "Email", fontSize: 18),
+          CustomText(textString: "Phone", fontSize: 18),
+          CustomText(textString: "Balance", fontSize: 18),
+          CustomText(textString: "Point", fontSize: 18),
+          CustomText(textString: "EXP", fontSize: 18),
+          CustomText(textString: "Reward", fontSize: 18),
         ],
       ),
     );
