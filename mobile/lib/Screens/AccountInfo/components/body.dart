7c564624ea2c4eac3898b54acd50ea94e795df7c
13519001 Karlsen Adiyasa Bachtiar <13519001@std.stei.itb.ac.id>
2022-04-14 01:46:44 +0700

    FE Account Info Screen done, Logout feature done
    Former-commit-id: 9873b0e796ffabf2d29759a3ac1b017d19418403

--- mobile/lib/Screens/AccountInfo/components/body.dart
+++ mobile/lib/Screens/AccountInfo/components/body.dart
@@ -1,7 +1,8 @@
+import 'package:cakrawala_mobile/Screens/Welcome/welcome_screen.dart';
 import 'package:cakrawala_mobile/components/custom_app_bar.dart';
 import 'package:cakrawala_mobile/constants.dart';
-import 'package:cakrawala_mobile/Screens/AccountInfo/components/custom_text.dart';
 import 'package:cakrawala_mobile/utils/userinfo-api.dart';
+import 'package:cakrawala_mobile/value-store/sp-handler.dart';
 import "package:flutter/material.dart";
 
 class Body extends StatefulWidget {
@@ -12,15 +13,23 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  var sp = SharedPreferenceHandler();
   String email = "-";
   String name = "-";
   String phone = "-";
   String balance = "-";
   String points = "-";
-  String exp = "-";
-  // String rewards = "-";
-  // TODO reward pake button
-  Map<String, dynamic> userData = {};
+  String serviceLevel = "-";
+  
+  List<String> userDataHeader = [
+    "Name",
+    "Email",
+    "Phone",
+    "Balance",
+    "Points",
+    "Service Level"
+  ];
+  List<String> userData = [];
 
   @override
   void initState() {
@@ -38,13 +47,43 @@ class _BodyState extends State<Body> {
             phone = data.data['Phone'];
             balance = data.data['balance'].toString();
             points = data.data['point'].toString();
-            exp = data.data['exp'].toString();
-            userData = data.data;
-            print(data.data);
+            serviceLevel = getUserLevelService(data.data['exp']).toString();
+            loadUserData();
           });
         }
       });
     });
+  }
+
+  void loadUserData() {
+    userData.add(name);
+    userData.add(email);
+    userData.add(phone);
+    userData.add(balance);
+    userData.add(points);
+    userData.add(serviceLevel);
+  }
+
+  void logOut() {
+    sp.revokeToken();
+  }
+
+  int getUserLevelService(int exp) {
+    if (exp > 2500) {
+      return 6;
+    } else if (exp > 1800) {
+      return 5;
+    } else if (exp > 1000) {
+      return 4;
+    } else if (exp > 600) {
+      return 3;
+    } else if (exp > 300) {
+      return 2;
+    } else if (exp > 90) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
 
   @override
@@ -58,14 +97,56 @@ class _BodyState extends State<Body> {
         backButton: false,
       ),
       body: Column(
-        children: const <Widget>[
-          CustomText(textString: "Name", fontSize: 18),
-          CustomText(textString: "Email", fontSize: 18),
-          CustomText(textString: "Phone", fontSize: 18),
-          CustomText(textString: "Balance", fontSize: 18),
-          CustomText(textString: "Point", fontSize: 18),
-          CustomText(textString: "EXP", fontSize: 18),
-          CustomText(textString: "Reward", fontSize: 18),
+        children: [
+          // PROFILE INFO
+          ListView.separated(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            separatorBuilder: (context, index) {
+              return const Divider(
+                color: Colors.white,
+              );
+            },
+            itemCount: userData.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(userDataHeader[index]),
+                subtitle: Text(
+                  userData[index],
+                  style: const TextStyle(
+                    fontFamily: 'Mulish',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: white,
+                  ),
+                ),
+              );
+            },
+          ),
+
+          // LOGOUT BUTTON
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            width: size.width * 0.27,
+            decoration: BoxDecoration(
+                color: Colors.red, borderRadius: BorderRadius.circular(50)),
+            child: TextButton.icon(
+              icon: const Icon(Icons.logout, color: white),
+              label: const Text("Log Out",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Mulish',
+                      fontSize: 15,
+                      fontWeight: FontWeight.w700)),
+              onPressed: () {
+                logOut();
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                      builder: (context) => const WelcomeScreen()),
+                );
+              },
+            ),
+          )
         ],
       ),
     );
