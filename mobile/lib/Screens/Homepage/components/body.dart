f754cbcd25778626cd0dde2923c7cd38128425af
Jesica <13519011@std.stei.itb.ac.id>
2022-04-14 13:26:41 +0700

    styling transaction history
    Former-commit-id: e88f58ea4b5ab9f4ad1b6dfbecf67b31fd8f6c1e

--- mobile/lib/Screens/Homepage/components/body.dart
+++ mobile/lib/Screens/Homepage/components/body.dart
@@ -43,7 +43,7 @@ class _BodyState extends State<Body> {
         const WhiteFieldContainer(
           child: WalletInfo(),
         ),
-        SizedBox(height: .017 * size.height,),
+        SizedBox(height: .006 * size.height,),
         Row(
           children: const [
             Padding(
