6797d2c610f59a07754088b4115f9d47e001cfcd
haikallf <haikallzrd@gmail.com>
2022-04-18 21:55:18 +0700

    fix language inconsistencies
    Former-commit-id: 0d26bf2e000d26a2a95388ac6d9dc5ec8633e9ae

--- admin/src/sections/@dashboard/products/ProductCard.js
+++ admin/src/sections/@dashboard/products/ProductCard.js
@@ -1,31 +1,26 @@
-import PropTypes from 'prop-types';
-import { Link as RouterLink } from 'react-router-dom';
+import PropTypes from "prop-types";
+import { Link as RouterLink } from "react-router-dom";
 // material
-import {Box, Card, Link, Typography, Stack, Button} from '@mui/material';
-import { styled } from '@mui/material/styles';
-// utils
-import { fCurrency } from '../../../utils/formatNumber';
+import { Box, Card, Link, Typography, Stack, Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
 //
-import Label from '../../../components/Label';
-import ColorPreview from '../../../components/ColorPreview';
-import Iconify from "../../../components/Iconify";
 import axios from "axios";
-import {url} from "../../../api";
+import { url } from "../../../api";
 
 // ----------------------------------------------------------------------
 
-const ProductImgStyle = styled('img')({
+const ProductImgStyle = styled("img")({
   top: 0,
-  width: '100%',
-  height: '100%',
-  objectFit: 'cover',
-  position: 'absolute'
+  width: "100%",
+  height: "100%",
+  objectFit: "cover",
+  position: "absolute",
 });
 
 // ----------------------------------------------------------------------
 
 ShopProductCard.propTypes = {
-  product: PropTypes.object
+  product: PropTypes.object,
 };
 
 export default function ShopProductCard({ product }) {
@@ -33,29 +28,31 @@ export default function ShopProductCard({ product }) {
 
   const stockEditor = (id) => {
     const newStock = window.prompt("Stock baru anda");
-    if(id && newStock && parseInt(newStock)>=0){
-      axios.patch(url + `/admin/reward/${id}`, {
-        stock : parseInt(newStock)
-      }).then(r=>{
-        if(r.status === 200){
-          alert("berhasil edit stok id: "+id)
-          window.location.reload()
-          return
-        }
-        alert("gagal update stok")
-      })
-      .catch(e=>{
-        console.log("updatestok", e)
-        alert("terjadi kesalahan saat update stok")
-      })
-    }else{
-      alert("invalid input!")
+    if (id && newStock && parseInt(newStock) >= 0) {
+      axios
+        .patch(url + `/admin/reward/${id}`, {
+          stock: parseInt(newStock),
+        })
+        .then((r) => {
+          if (r.status === 200) {
+            alert("Stock has been updated with id: " + id);
+            window.location.reload();
+            return;
+          }
+          alert("ERROR: Stock cannot be updated");
+        })
+        .catch((e) => {
+          console.log("updatestok", e);
+          alert("ERROR: Something went wrong");
+        });
+    } else {
+      alert("ERROR: invalid input!");
     }
-  }
+  };
 
   return (
     <Card>
-      <Box sx={{ pt: '100%', position: 'relative' }}>
+      <Box sx={{ pt: "100%", position: "relative" }}>
         <ProductImgStyle alt={name} src={image} />
       </Box>
 
@@ -66,14 +63,18 @@ export default function ShopProductCard({ product }) {
           </Typography>
         </Link>
 
-        <Stack direction="row" alignItems="center" justifyContent="space-between">
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+        >
           <Typography variant="subtitle1">
             <Typography
-                component="span"
-                variant="body1"
-                sx={{
-                  color: 'text.disabled'
-                }}
+              component="span"
+              variant="body1"
+              sx={{
+                color: "text.disabled",
+              }}
             >
               Stock:
             </Typography>
@@ -85,7 +86,7 @@ export default function ShopProductCard({ product }) {
               component="span"
               variant="body1"
               sx={{
-                color: 'text.disabled',
+                color: "text.disabled",
               }}
             >
               Pts
@@ -96,12 +97,14 @@ export default function ShopProductCard({ product }) {
         </Stack>
 
         <Button
-            fullWidth
-            size="large"
-            type="submit"
-            color="inherit"
-            variant="outlined"
-            onClick={()=>{stockEditor(id)}}
+          fullWidth
+          size="large"
+          type="submit"
+          color="inherit"
+          variant="outlined"
+          onClick={() => {
+            stockEditor(id);
+          }}
         >
           Edit Stock
         </Button>
