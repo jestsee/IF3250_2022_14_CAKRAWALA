8d1b44fa224131ee1f24e0106a5b5891270e9c9f
haikallf <haikallzrd@gmail.com>
2022-04-28 00:05:42 +0700

    build configuration
    Former-commit-id: 981a9e913f04a5cba7fc445a19390956dd8c2df7

--- mobile/lib/value-store/constant.dart
+++ mobile/lib/value-store/constant.dart
@@ -1,4 +1,4 @@
 class Constant {
-  //static String URL_BE = "http://34.87.134.120:8080/v1";
-  static String URL_BE = "http://192.168.1.14:8080/v1";
+  static String URL_BE = "http://34.87.134.120:8080/v1";
+  // static String URL_BE = "http://192.168.1.14:8080/v1";
 }
